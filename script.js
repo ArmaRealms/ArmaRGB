e126f13b7b14847a0f2097610be549ee9e79db47
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2024-07-17 11:31:10 -0300

    Marcar página como escura por padão

--- script.js
+++ script.js
@@ -67,39 +67,23 @@ const formats = {
   },
 };
 
-function darkMode() {
-  if (document.getElementById('darkmode').checked == true) {
-    document.body.classList.add('dark');
-    document.getElementById('output-format').classList.add("dark");
-    document.getElementById('color-preset').classList.add("dark");
-    document.getElementById('numOfColors').classList.add("dark");
-    document.getElementById('graylabel1').classList.replace("gray", "darkgray");
-    document.getElementById('graylabel2').classList.replace("gray", "darkgray");
-    document.getElementById('outputText').classList.replace("gray", "darkgray");
-    document.getElementById('outputText').classList.replace("gray", "darkgray");
-    document.getElementById('error').classList.replace("errortext", "darkerrortext");
-    document.getElementById('numOfColors').classList.add("darktextboxes");
-    document.getElementById('nickname').classList.add("darktextboxes");
-    document.getElementById('outputText').classList.add("darktextboxes");
-    Array.from(document.getElementsByClassName("hexColor")).forEach(e => {
-      document.getElementById(e.id).classList.add("darktextboxes");
-    })
-  } else {
-    document.body.classList.remove('dark');
-    document.getElementById('output-format').classList.remove("dark");
-    document.getElementById('color-preset').classList.remove("dark");
-    document.getElementById('numOfColors').classList.remove("dark");
-    document.getElementById('graylabel1').classList.replace("darkgray", "gray");
-    document.getElementById('graylabel2').classList.replace("darkgray", "gray");
-    document.getElementById('outputText').classList.replace("darkgray", "gray");
-    document.getElementById('error').classList.replace("darkerrortext", "errortext");
-    document.getElementById('numOfColors').classList.remove("darktextboxes");
-    document.getElementById('nickname').classList.remove("darktextboxes");
-    document.getElementById('outputText').classList.remove("darktextboxes");
-    Array.from(document.getElementsByClassName("hexColor")).forEach(e => {
-      document.getElementById(e.id).classList.remove("darktextboxes");
-    })
-  }
+// Set dark mode by default
+function setDarkMode() {
+  document.body.classList.add('dark');
+  document.getElementById('output-format').classList.add("dark");
+  document.getElementById('color-preset').classList.add("dark");
+  document.getElementById('numOfColors').classList.add("dark");
+  document.getElementById('graylabel1').classList.replace("gray", "darkgray");
+  document.getElementById('graylabel2').classList.replace("gray", "darkgray");
+  document.getElementById('outputText').classList.replace("gray", "darkgray");
+  document.getElementById('outputText').classList.replace("gray", "darkgray");
+  document.getElementById('error').classList.replace("errortext", "darkerrortext");
+  document.getElementById('numOfColors').classList.add("darktextboxes");
+  document.getElementById('nickname').classList.add("darktextboxes");
+  document.getElementById('outputText').classList.add("darktextboxes");
+  Array.from(document.getElementsByClassName("hexColor")).forEach(e => {
+    document.getElementById(e.id).classList.add("darktextboxes");
+  });
 }
 
 /* Get a random HEX color */
@@ -371,7 +355,7 @@ function preset(n) {
     }
     jscolor.install(); // Refresh all jscolor elements
 }
+
 toggleColors(2);
 updateOutputText();
-document.getElementById('darkmode').checked = true
-darkMode()
+setDarkMode()
